ac4ddd478e13e93bad1cc980eaff530c8dfef0fa
FatimaMatrook <202006773@stu.uob.edu.bh>
2024-11-02 14:04:03 +0300

    Update

--- php/login.php
+++ php/login.php
@@ -3,18 +3,128 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>IT Room Booking System</title>
+
     <style>
         
 body {
     margin: 0;
     padding: 0;
     font-family: Arial, sans-serif;
-    background-color: #f4f4f4;
     height: 100vh; 
     display: flex; 
     justify-content: center;
     align-items: center; 
+   
+}
+:root{
+    --first-color:#243642;
+    --seconed-color: #387478;
+    --third-color:#629584;
+    --forth-color: #E2F1E7;
+}
+html{
+    font-size: 62.5%;
+    overflow-x: hidden;
+    scroll-padding-top: 9rem;
+    scroll-behavior: smooth;
+}
+
+html::-webkit-scrollbar{
+    width: .8rem;
+}
+html::-webkit-scrollbar-track{
+    background: transparent;
+}
+html::-webkit-scrollbar-thumb{
+    background: #ffffff;
+    border-radius: 5rem;
+}
+body{
+    background: var(--forth-color);
+}
+section{
+    padding: 2rem 7%;
+}
+.btn{
+    margin-top: 1rem;
+    display: inline-block;
+    padding: .9rem 3rem;
+    font-size: 1.7rem;
+    color: #fff;
+    background:var(--first-color) ;
+    cursor: pointer;
+}
+.btn:hover{
+    letter-spacing: .1rem;
+}
+.header{
+    background: var(--forth-color);
+    display: flex;
+    align-items: center;
+    justify-content: space-between ;
+    padding: 1.5rem 7%;
+    border-bottom: var(--first-color);
+    position: fixed;
+    top: 0; left: 0; right: 0;
+    z-index: 1000;
+}
+.header .logo img{
+    height: 6rem;
+}
+.header .navbar a{
+    margin:0 1rem;
+    font-size: 1.6rem;
+    color: var(--first-color);
+}
+.header .navbar a:hover{
+    color: var(--seconed-color);
+    border-bottom: .1rem solid var(--seconed-color);
+    padding-bottom: .5rem;
+}
+.header .icons div{
+    color: var(--first-color);
+    cursor: pointer;
+    font-size: 2.5rem;
+    margin-left: 2rem;
+}
+.header .icons div:hover{
+    color: var(--seconed-color);
+}
+#menu-btn{
+    display: none;
+}
+.header .search-form{
+    position: absolute;
+    top: 115%; right: 7%;
+    background: var(--forth-color);
+    width: 50rem;
+    height: 5rem;
+    display: flex;
+    align-items: center;
+    transform: scaleY(0);
+    transform-origin: top;
+}
+.header .search-form.active{
+    transform: scaleY(1);
+
+}
+.header .search-form input{
+    height: 100%;
+    width: 100%;
+    font-size: 1.6rem;
+    color: #000;
+    padding: 1rem;
+    text-transform: none;
+}
+.header .search-form label{
+    cursor: pointer;
+    font-size: 2.2rem;
+    margin-right: 1.5rem;
+    color: #000;
+}
+.header .search-form label:hover{
+  color: var(--first-color);
 }
 
 /* Header style */
@@ -31,7 +141,7 @@ h2 {
 
 /* Form style */
 form {
-    background-color: white; 
+    background: var(--forth-color); 
     padding: 20px; 
     border-radius: 8px; 
     box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); 
@@ -64,7 +174,7 @@ input[type="text"] {
 .btn {
     width: 100%; 
     padding: 10px; 
-    background-color: #007bff; 
+    background: var(--third-color);
     color: white; 
     border: none; 
     border-radius: 4px; 
@@ -73,12 +183,13 @@ input[type="text"] {
 
 /* Button hover effect */
 .btn:hover {
-    background-color: #0056b3; }
+    background: var(--secound-color);}
 
 /* Style for the sign-up link */
 p {
     text-align: center; 
-    margin-top: 15px; }
+    margin-top: 15px;
+    color: var(--secound-color);; }
 
 a {
     color: #007bff; 
@@ -90,7 +201,35 @@ a:hover {
 </style>
 </head>
 <body>
-   
+    <!--Header start-->    
+    <header class="header">
+        
+        <!-- this is for  uob logo-->
+        <a href="#" class="logo"><img src="images/UOB LOGO.png" alt="uob logo"></a>
+        <!-- this is for  uob logo end-->    
+        
+        <!--navbar-->
+        <nav class="navbar">
+            <a href="#home">Home</a>
+            <a href="#about">About us</a>
+            <a href="php/login.php">Log in</a>
+            <a href="#rooms">Rooms</a>
+            <a href="#contact">Contact us</a>
+            <a href="#reviews">Reviews</a>
+        </nav>
+        <!--navbar end-->
+        <div class="icons">
+            <div class="fas fa-search" id="search-btn"></div>
+            <div class="fas fa-bars" id="menu-btn"></div>
+        </div>
+
+        <div class="search-form">
+            <input type="search" id="search-box" placeholder="search here...">
+            <label for="search-box" class="fas fa-search"></label>
+        </div>
+    </header>
+    <!--Header end-->
+
     <form method="post" action="login(1).php">
         
         <div class="input-group">
--- php/register.php
+++ php/register.php
@@ -14,6 +14,115 @@ body {
     justify-content: center; 
     align-items: center; 
 }
+:root{
+    --first-color:#243642;
+    --seconed-color: #387478;
+    --third-color:#629584;
+    --forth-color: #E2F1E7;
+}
+html{
+    font-size: 62.5%;
+    overflow-x: hidden;
+    scroll-padding-top: 9rem;
+    scroll-behavior: smooth;
+}
+
+html::-webkit-scrollbar{
+    width: .8rem;
+}
+html::-webkit-scrollbar-track{
+    background: transparent;
+}
+html::-webkit-scrollbar-thumb{
+    background: #ffffff;
+    border-radius: 5rem;
+}
+body{
+    background: var(--forth-color);
+}
+section{
+    padding: 2rem 7%;
+}
+.btn{
+    margin-top: 1rem;
+    display: inline-block;
+    padding: .9rem 3rem;
+    font-size: 1.7rem;
+    color: #fff;
+    background:var(--first-color) ;
+    cursor: pointer;
+}
+.btn:hover{
+    letter-spacing: .1rem;
+}
+.header{
+    background: var(--forth-color);
+    display: flex;
+    align-items: center;
+    justify-content: space-between ;
+    padding: 1.5rem 7%;
+    border-bottom: var(--first-color);
+    position: fixed;
+    top: 0; left: 0; right: 0;
+    z-index: 1000;
+}
+.header .logo img{
+    height: 6rem;
+}
+.header .navbar a{
+    margin:0 1rem;
+    font-size: 1.6rem;
+    color: var(--first-color);
+}
+.header .navbar a:hover{
+    color: var(--seconed-color);
+    border-bottom: .1rem solid var(--seconed-color);
+    padding-bottom: .5rem;
+}
+.header .icons div{
+    color: var(--first-color);
+    cursor: pointer;
+    font-size: 2.5rem;
+    margin-left: 2rem;
+}
+.header .icons div:hover{
+    color: var(--seconed-color);
+}
+#menu-btn{
+    display: none;
+}
+.header .search-form{
+    position: absolute;
+    top: 115%; right: 7%;
+    background: var(--forth-color);
+    width: 50rem;
+    height: 5rem;
+    display: flex;
+    align-items: center;
+    transform: scaleY(0);
+    transform-origin: top;
+}
+.header .search-form.active{
+    transform: scaleY(1);
+
+}
+.header .search-form input{
+    height: 100%;
+    width: 100%;
+    font-size: 1.6rem;
+    color: #000;
+    padding: 1rem;
+    text-transform: none;
+}
+.header .search-form label{
+    cursor: pointer;
+    font-size: 2.2rem;
+    margin-right: 1.5rem;
+    color: #000;
+}
+.header .search-form label:hover{
+  color: var(--first-color);
+}
 
 /* Center the container */
 .container {
@@ -76,6 +185,35 @@ label {
 
 </head>
   <body>
+    <!--Header start-->    
+    <header class="header">
+        
+        <!-- this is for  uob logo-->
+        <a href="#" class="logo"><img src="images/UOB LOGO.png" alt="uob logo"></a>
+        <!-- this is for  uob logo end-->    
+        
+        <!--navbar-->
+        <nav class="navbar">
+            <a href="#home">Home</a>
+            <a href="#about">About us</a>
+            <a href="php/login.php">Log in</a>
+            <a href="#rooms">Rooms</a>
+            <a href="#contact">Contact us</a>
+            <a href="#reviews">Reviews</a>
+        </nav>
+        <!--navbar end-->
+        <div class="icons">
+            <div class="fas fa-search" id="search-btn"></div>
+            <div class="fas fa-bars" id="menu-btn"></div>
+        </div>
+
+        <div class="search-form">
+            <input type="search" id="search-box" placeholder="search here...">
+            <label for="search-box" class="fas fa-search"></label>
+        </div>
+    </header>
+    <!--Header end-->
+    
     <div class="container">
             <h1>Registration</h1>
           </div>
